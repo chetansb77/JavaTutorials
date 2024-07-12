6b363a4fd3d8e129c33f185002fd0cef794b9317
Chetan Bhat <35666818+chetansb77@users.noreply.github.com>
2024-07-13 00:30:03 +0530

    initial commit

---
+++ DataTypes.java
@@ -0,0 +1,21 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        byte b = 10;
+        short s = 20;
+        int i = 30;
+        long l = 40;
+        float f = 50.0f;
+        double d = 60.0;
+        char c = 'a';
+        boolean bool = true;
+
+        System.out.println("byte: " + b);
+        System.out.println("short: " + s);
+        System.out.println("int: " + i);
+        System.out.println("long: " + l);
+        System.out.println("float: " + f);
+        System.out.println("double: " + d);
+        System.out.println("char: " + c);
+        System.out.println("boolean: " + bool);
+    }
+}
---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+class HelloWorld {
+    /* This is an example of  multi-line comment.
+    * The program prints "Hello, World!" to the standard output.
+    */
+    public static void main(String[] args) {
+         // print Hello World to the screen
+        String message = "Hello, World!";
+        System.out.println(message); 
+        message = "Welcome to Java!";
+        System.out.println(message); 
+    }
+}
